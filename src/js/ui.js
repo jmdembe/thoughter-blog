e61a8fc267a66584ac0c37c57ed7024830e5ee6e
Jessica Dembe <jmdembe@gmail.com>
2016-11-09 14:35:28 -0500

    Re-named nav to ui, created nav on index

---
+++ src/js/ui.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+    console.log("Page load");
+    window.thoughter = window.thoughter || {};
+
+    window.addEventListener('hashchange', function(event) {
+        $('section').hide()
+        console.log('this works!', window.location.hash);
+        $(window.location.hash).show()
+    })
+})();
