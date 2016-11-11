5798ec22aa13cee15f66e50a6d120e22f7eeafb4
Jessica Dembe <jmdembe@gmail.com>
2016-11-11 13:00:23 -0500

    Added text area and submit button to index, removed global variable on nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,8 +3,6 @@
     console.log("Page load");
     window.thoughter = window.thoughter || {};
 
-    // window.thoughter.messageList = messageList;
-
     window.addEventListener('hashchange', function(event) {
         $('section').hide()
         console.log('this works!', window.location.hash);
