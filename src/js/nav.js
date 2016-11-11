160ce22f2aa5fb490846d7785e35cfe7ceeb8746
Jessica Dembe <jmdembe@gmail.com>
2016-11-11 12:51:33 -0500

    Added text area to index and edited a line from nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,7 +3,7 @@
     console.log("Page load");
     window.thoughter = window.thoughter || {};
 
-    window.thoughter.messageList = messageList;
+    // window.thoughter.messageList = messageList;
 
     window.addEventListener('hashchange', function(event) {
         $('section').hide()
