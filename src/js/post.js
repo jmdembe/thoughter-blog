293d63100832b1a8f83f85ab97d5f6641514f132
Jessica Dembe <jmdembe@gmail.com>
2016-11-12 22:38:55 -0500

    Added functionality to create new thoughts

--- src/js/post.js
+++ src/js/post.js
@@ -2,25 +2,29 @@
     'use strict';
 
     window.thoughter = window.thoughter || {};
-    window.thoughter.newThought = newThought;
+    // window.thoughter.newThought = newThought;
 
-    function newThought() {
-        return $.ajax({
-            url: 'https://thoughter.herokuapp.com/api/Thoughts',
-            method: 'POST',
-            dataType: 'json',
-            headers: {
-                'id': Number,
-                'content': String,
-                'createTime': String,
-            }
-            .done (function processPostSuccess(data) {
-                console.log ("post success", data);
-            })
-            .fail (function processPostError(xhr) {
-                console.log ("Error", xhr);
-            })
+    var content = $('.form-control').val();
 
-        });
-    }
+    window.addEventListener('submit', function(event){
+      event.preventDefault();
+      $.ajax({
+          url: 'https://thoughter.herokuapp.com/api/Thoughts',
+          method: 'POST',
+          data: {
+              "id": Number,
+              "content": content,
+              "createTime": String,
+
+          }
+          // .done(function processPostSuccess(data) {
+          //    console.log ("post success", data);
+          // })
+          // .fail(function processPostError(xhr) {
+          //    console.log ("Error", xhr);
+          // })
+      });
+      console.log("submit button");
+    });
+
 }());
