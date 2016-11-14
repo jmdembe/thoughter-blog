f848556082612ecf954cbd373e2a9165b732b194
Jessica Dembe <jmdembe@gmail.com>
2016-11-14 15:28:32 -0500

    Placed data collection info in one file, made updates and edits to test files

--- src/js/data.js
+++ src/js/data.js
@@ -4,24 +4,56 @@
     window.thoughter = window.thoughter || {};
     window.thoughter.thoughtsList = thoughtsList;
 
+
     /**
-     * Purpose: Retrieve a list of all thoughts from the server.
-     * @return {Promise}  The Ajax call promise
+     * function that grabs thoughts from the API
+     * @param  {Number} filter
+     * @return {Promise}        Promise from ajax call
      */
+
     function thoughtsList() {
-        return $.ajax({
-            url: 'https://thoughter.herokuapp.com/api/Thoughts?filter={"limit":10}',
-            method: 'GET',
-            dataType: 'json',
-        })
-        .done(function processThoughts(data){
-            console.log("Information obtained", data);
-        })
-        .fail(function processErrorThoughts(xhr) {
-            console.log('The submission did not go through', xhr);
-            $('ul')
-              .append('<li>The submission did not go through</li>')
+      return $.ajax({
+          url: 'https://thoughter.herokuapp.com/api/Thoughts?filter={"limit":10}',
+          method: 'GET',
+          dataType: 'json',
+      })
+      .done(function processThoughts(data){
+          console.log("Information obtained", data);
+      })
+      .fail(function processErrorThoughts(xhr) {
+          console.log('The submission did not go through', xhr);
+          $('ul')
+            .append('<li>The submission did not go through</li>')
         })
       }
 
+      /**
+       * newThought enters a new thought after the user hits submit
+       * @param  {String} content This is the new thought
+       * @return {Promise}         Ajax promise
+       */
+
+      function newThought(content) {
+        if (typeof(id)!== 'string') {
+            var dfdObj = $.Deferred();
+            dfdObj.reject('I need content!')
+            return dfdObj.promise();
+        }
+        return $.ajax({
+            url: 'https://thoughter.herokuapp.com/api/Thoughts',
+              method: 'POST',
+              dataType: 'json',
+              data:JSON.stringify({'content': content}),
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          })
+          .done (function newThoughtSuccess(data) {
+              console.log(data);
+          })
+          .fail (function newThoughtError(xhr) {
+              console.log('Try again', xhr);
+          })
+
+        }
 })();
---
+++ test/specs/post-newthought.spec.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    var expect = chai.expect;
+
+    describe('Posting a new thought')
+      it('Should connect with the server to post a new thought') {
+
+        var result = window.thoughter.newThought();
+        expect(result).to.be.an('object');
+        expect(result.done).to.be.a('function');
+        expect(result.fail).to.be.a('function');
+
+        result
+          .done(function() {
+            it('should indicate that the function is completed', function(asyncComplete){
+                expect(asyncComplete).to.be.a('function');
+            })
+
+            it('should use an array to append thoughts as a list item', function (complete){
+              var testArray = function buildThoughts ("listOfThoughts");
+
+            })
+            it('Should append items to page upon page load', function(asyncComplete) {
+                expect(asyncComplete).to.be.a('function');
+            })
+
+            }
+            asyncComplete();
+          })
+          .fail(function() {
+            asyncComplete();
+          })
+      }
+
+}());
