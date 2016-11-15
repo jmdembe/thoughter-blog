5c87614cf6d19ce1ee47f79118716c1520e557ce
Jessica Dembe <jmdembe@gmail.com>
2016-11-14 19:27:58 -0500

    Added new function to add thoughts

--- src/js/newthought-view.js
+++ src/js/newthought-view.js
@@ -1,21 +1,33 @@
 (function() {
     'use strict';
 
+    window.thoughter = window.thoughter || {};
 
     $('.new-thought')
-      .on('submit', function addThought(eventObj) {
-          eventObj.preventDefault();
-
-          console.log('hi');
-
+      .on('submit', function addThought() {
+        console.log(1);
           window.thoughter.newThought($('.form-control').val())
             .done(function addNewThought () {
                 window.location.hash='#recentthoughts';
+
             })
             .fail(function errorHandler(xhr) {
                 console.log('Why are you failing?', xhr);
             })
       })
+
+      /**
+       * Lists a new thought to the recent thoughts
+       * @param  {Array} thought The thought that the user submits
+       */
+      function listNewThought (thoughts) {
+        thoughts.forEach(thought) {
+          $('.thoughts')
+              .append(
+                  '<li class panel panel-default>'+ thought.content +'</li>'
+              )
+        }
+      };
 
 
 
