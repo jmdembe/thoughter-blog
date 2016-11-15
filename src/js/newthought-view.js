6d6eec56d309be5db1b1ed5dadd8783a0cd43a23
Jessica Dembe <jmdembe@gmail.com>
2016-11-14 20:10:11 -0500

    Added doc info to build thoughts function, made edits to newthought-views

--- src/js/newthought-view.js
+++ src/js/newthought-view.js
@@ -2,32 +2,20 @@
     'use strict';
 
     window.thoughter = window.thoughter || {};
-
     $('.new-thought')
       .on('submit', function addThought() {
-        console.log(1);
           window.thoughter.newThought($('.form-control').val())
-            .done(function addNewThought () {
+            .done(function addNewThought (data) {
                 window.location.hash='#recentthoughts';
-
+                console.log('did done work');
+                window.thoughter.buildThoughts(data);
             })
             .fail(function errorHandler(xhr) {
                 console.log('Why are you failing?', xhr);
             })
       })
 
-      /**
-       * Lists a new thought to the recent thoughts
-       * @param  {Array} thought The thought that the user submits
-       */
-      function listNewThought (thoughts) {
-        thoughts.forEach(thought) {
-          $('.thoughts')
-              .append(
-                  '<li class panel panel-default>'+ thought.content +'</li>'
-              )
-        }
-      };
+
 
 
 
--- src/js/thoughts-view.js
+++ src/js/thoughts-view.js
@@ -5,10 +5,21 @@
 
     window.thoughter.buildThoughts = buildThoughts;
 
+
+    /**
+     * [buildThoughts description]
+     * @param  {Array} listOfThoughts Passes a list of thoughts through the function
+     */
     function buildThoughts (listOfThoughts) {
+        if (!Array.isArray(listofThoughts)) {
+          return;
+        }
+
         listOfThoughts.forEach(function appendingThoughtsToAList (thought) {
         $('ul.thoughts')
-            .append('<li>'+ thought.content+'</li>')
+            .append(
+              '<li class=panel panel-primary>'+ thought.content+ thought.date +'</li>'
+            )
         });
 
     }
