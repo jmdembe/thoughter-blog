3b656cdbd9482c1c11f4df2ffec9596cf8d4cfea
Jessica Dembe <jmdembe@gmail.com>
2016-11-22 10:46:56 -0500

    Fixed indentation in data spec and fixed a tet in post-newthought spec

--- test/specs/data.spec.js
+++ test/specs/data.spec.js
@@ -4,30 +4,24 @@
     var expect = chai.expect;
 
     describe('thought data module', function() {
-        it('Should expect the namespace exists', function (){
-            expect(window.thoughter).to.be.an('object');
+      it('Should expect the namespace exists', function (){
+          expect(window.thoughter).to.be.an('object');
+      });
+
+      describe('getting list of thoughts', function() {
+
+        it('should obtain list from the server from thoughtsList', function(asyncComplete) {
+
+          var result = window.thoughter.thoughtsList();
+          expect(result).to.be.an('object');
+          expect(result.done).to.be.a('function');
+          expect(result.fail).to.be.a('function');
+
+          result
+            .done(function() {
+
+            });
         });
-
-        describe('getting list of thoughts', function() {
-
-          it('should obtain list from the server', function(asyncComplete) {
-
-            var result = window.thoughter.thoughtsList();
-            expect(result).to.be.an('object');
-            expect(result.done).to.be.a('function');
-            expect(result.fail).to.be.a('function');
-
-            result
-              .done(function() {
-                it('should indicate that the function is completed', function(asyncComplete){
-                    expect(asyncComplete).to.be.a('function');
-                });
-                asyncComplete();
-              })
-              .fail(function() {
-                asyncComplete();
-              });
-          });
-        });
-    });
+      });
+  });
 }());
